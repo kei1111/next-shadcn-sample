b1c266d25fb51479f2eee10437de02a7ed9513d0
kei1111 <ke1taki6a25@outlook.jp>
2023-07-10 23:31:46 +0900

    import clerk

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
+const SetupPage = () => {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+};
+
+export default SetupPage;
